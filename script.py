3b98d5e436f9f1618a5d3d0db8d710471ae48655
cowboymivina <radiognome.invisible@gmail.com>
2022-11-18 16:26:34 +0100

    changed the functionality to meet the requirements: now multiple csvs are being created fore each book category instead of one csv that contains every book

--- script.py
+++ script.py
@@ -2,71 +2,90 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
-#creating a csv and writing data from headers and contents list
-#creating headers list
-headers = ["product_page_url", "universal_ product_code (upc)", "book_title", "price_including_tax", "price_excluding_tax", "quantity_available", "product_description", "category", "review_rating", "image_url"]
-
-#creating the csv file and writing headers
-with open('result.csv', 'w', encoding="utf-8", newline='') as csvfile:
-    #creating a writer object with the file
-    writer = csv.writer(csvfile)
-    writer.writerow(headers)
-
-#url of the website to scrape
+#making soup out of the main page of website to scrape
 url = "http://books.toscrape.com/"
 main_page = requests.get(url)
 
 soup = BeautifulSoup(main_page.content, "html.parser")
 
-#getting the list of categories from main page
+#getting the list of book categories from main page
 categories_list = []
 
 for data in soup.find_all(class_="side_categories"):
     for a in data("a"):
-        link = (a.get('href'))
+        link = (a.get("href"))
         categories_list.append(link.replace("catalogue/", "http://books.toscrape.com/catalogue/"))
 
-#removing the All books category from the list as we don't need it
+#removing the "All books" category from the list as having it will produce dupes
 categories_list.remove("http://books.toscrape.com/catalogue/category/books_1/index.html")
+
+#creating headers list
+headers = ["product_page_url", "universal_ product_code (upc)", "book_title", "price_including_tax", "price_excluding_tax", "quantity_available", "product_description", "category", "review_rating", "image_url"]
+
+#getting the list of book categories from main page
+categories_list = []
+
+for data in soup.find_all(class_="side_categories"):
+    for a in data("a"):
+        link = (a.get("href"))
+        categories_list.append(link.replace("catalogue/", "http://books.toscrape.com/catalogue/"))
+
+#removing the "All books" category from the list as having it will produce dupes
+categories_list.remove("http://books.toscrape.com/catalogue/category/books_1/index.html")
+
+#creating a directory for each category and putting an output csv file inside
+for cat_link in categories_list:
+    cat_url = requests.get(cat_link)
+    soup = BeautifulSoup(cat_url.content, "html.parser")
+    cat_name = soup.find(class_="active")
+    cat_list_name = cat_name.string
+    #creating a directory named after the category of the book
+    os.makedirs(f"c:/bookstoscrape/{cat_list_name}/", exist_ok=True)
+    #creating the output csv file and writing headers while making sure it's utf8-friendly
+    with open(f"c:/bookstoscrape/{cat_list_name}/{cat_list_name}.csv", "a", encoding="utf-8", newline='') as csvfile:
+    #creating a writer object with the file
+        writer = csv.writer(csvfile)
+        writer.writerow(headers)
+    #TODO close the file in the loop
 
 #creating a list of links to each separate book
 links = []
 
+#populating the list of links by going into each category and finding books links
 for category_link in categories_list:
-    #getting the cat html using requests
+    #getting the category html and making the soup out of it
     page = requests.get(category_link)
-
-    #transforming html into a soup object 
     soup = BeautifulSoup(page.content, "html.parser")
 
-    #explain h3 in the comment and why exactly i did it like this
+    #books links can be identified by the h3 tag in the html – this tag isn't used for anything else but the book links
     for data in soup.find_all("h3"):
-     for a in data:
-        link = (a.get('href'))
-        links.append(link.replace("../../../", "http://books.toscrape.com/catalogue/"))
-
+        for a in data:
+            link = (a.get('href'))
+            links.append(link.replace("../../../", "http://books.toscrape.com/catalogue/"))
+    
+    #making sure that categories with two or more pages of books are parsed correctly. the biggest category has 8 pages.
+    #TODO get the count of pages from html; change the range to end at var+1 so it doesn't run unnecessarily and that it's scalable
     pages = []
-    pages.append(category_link)
-    pages.append(category_link.replace("index.html", "page-2.html")) 
-    pages.append(category_link.replace("index.html", "page-3.html"))
-    pages.append(category_link.replace("index.html", "page-4.html"))
-    pages.append(category_link.replace("index.html", "page-5.html"))
-    pages.append(category_link.replace("index.html", "page-6.html"))
-    pages.append(category_link.replace("index.html", "page-7.html"))
-    pages.append(category_link.replace("index.html", "page-8.html"))
-
-    for i in pages:
-        nextpage = requests.get(i)
-        print(i)
-        soup = BeautifulSoup(nextpage.content, "html.parser")
-        for data in soup.find_all("h3"):
-            for a in data:
-                link = (a.get('href'))
-                links.append(link.replace("../../../", "http://books.toscrape.com/catalogue/"))   
+    page_number = list(range(2,10))
+    for number in page_number:
+        pages.append(category_link.replace("index.html", f"page-{number}.html"))
+    
+    #if a category has two or more pages, they will be parsed for book links which will be added to the "links" list
+    for page in pages:
+        nextpage = requests.get(page)
+        if nextpage.status_code == 404:
+            break
+        else:
+            soup = BeautifulSoup(nextpage.content, "html.parser")
+            for data in soup.find_all("h3"):
+                for a in data:
+                    link = (a.get('href'))
+                    links.append(link.replace("../../../", "http://books.toscrape.com/catalogue/"))   
 
 
-#extracting and writing information from separate book pages
+#extracting and writing information from the book pages
 for book_link in links:
     #getting the page html using requests
     page = requests.get(book_link)
@@ -74,7 +93,7 @@ for book_link in links:
     #transforming html into a soup object 
     soup = BeautifulSoup(page.content, "html.parser")
 
-    #creating a contents list from the text inside <td>
+    #creating a contents list from the text inside <td>: some required information is stored in a table, which can be identified by the td tag
     bs_contents = soup.find_all("td")
 
     contents = []
@@ -105,9 +124,8 @@ for book_link in links:
 
     #getting category
     bs_cat = soup.find_all("a")
-    cat = bs_cat[3]
-    category = cat.string
-
+    category = bs_cat[3].string
+    
     #getting review rating
     bs_ratings = soup.find("p", class_="star-rating").get("class")
     rating = bs_ratings[1]
@@ -115,16 +133,19 @@ for book_link in links:
     #getting image url
     imgurl = soup.find_all("img")[0].get("src").replace("../../", "http://books.toscrape.com/")
 
+    #saving the img file
+    filename = os.path.split(imgurl)[1]
+    r = requests.get(imgurl, stream=True)
+    if r.status_code == 200:
+        with open(f"c:/bookstoscrape/{category}/{filename}.jpg", 'wb') as f:
+            for chunk in r.iter_content(1024):
+                f.write(chunk)
+        #TODO close the img file
+
     #adding gathered info to the csv
-    with open('result.csv', 'a', encoding="utf-8") as csvfile:
+    with open(f"c:/bookstoscrape/{category}/{category}.csv", "a", encoding="utf-8") as csvfile:
     #creating a writer object with the file
         writer = csv.writer(csvfile, lineterminator='\n')
         row = [book_link, upc, booktitle, pricetax, pricenotax, quantity, description, category, rating, imgurl]
         writer.writerow(row)
-
-with open('result.csv', encoding="utf-8") as f:
-  data = list(csv.reader(f))
-  new_data = [a for i, a in enumerate(data) if a not in data[:i]]
-  with open('result.csv', 'w', encoding="utf-8") as t:
-     write = csv.writer(t, lineterminator='\n')
-     write.writerows(new_data)
+    #TODO close the file
